72fe361af69bf019a249b0d0d7af343e40bea679
diego23ac <corso.diegob@gmail.com>
2024-08-21 22:22:44 -0500

    add: tests.py

---
+++ 'TESTS'.py
@@ -0,0 +1,39 @@
+'TESTS'
+
+import OPERACIONES_DE_NUMEROS_COMPLEJOS as lc
+#SUMA
+# Caso 1:
+a = (3, 4)
+b = (1, 2)
+resultado = suma(a, b)
+# Resultado esperado: (4, 6)
+
+# Caso 2:
+a = (-2, 3)
+b = (5, -1)
+resultado = suma(a, b)
+# Resultado esperado: (3, 2)
+
+# Caso 1:
+a = (3, 4)
+b = (1, 2)
+resultado = resta(a, b)
+# Resultado esperado: (2, 2)
+
+# Caso 2:
+a = (-2, 3)
+b = (5, -1)
+resultado = resta(a, b)
+# Resultado esperado: (-7, 4)
+
+# Caso 1:
+a = (3, 4)
+b = (1, 2)
+resultado = multiplicacion(a, b)
+# Resultado esperado: (-5, 10)
+
+# Caso 2:
+a = (-2, 3)
+b = (5, -1)
+resultado = multiplicacion(a, b)
+# Resultado esperado: (-7, 17)
